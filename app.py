c19c9f19cfabbc61887c4299731ebd04fca7bcb4
Finger-cori <ojd9512@gmail.com>
2025-03-06 16:24:41 +0900

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, jsonify
-
-app = Flask(__name__)
-@app.route("/", methods=["GET"])
-def index():
-    return jsonify({"message": "Hello, FastCGI is working!"})
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
