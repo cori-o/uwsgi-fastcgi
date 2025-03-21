7eaac6b2dc5694cf029c8b074674431a9d0579e3
Finger-cori <ojd9512@gmail.com>
2025-03-21 13:58:36 +0900

    Update __init__.py
    delete LLMMistral Class

--- rag/src/__init__.py
+++ rag/src/__init__.py
@@ -1,4 +1,4 @@
-from .llm import EmbModel, LLMOpenAI, LLMMistral
+from .llm import EmbModel, LLMOpenAI
 from .query import QueryTranslator, QueryRouter, OpenAIQT, MistralQT, RulebookQR
 from .milvus import MilVus, MilvusEnvManager, DataMilVus, MilvusMeta
 from .data_p import DataProcessor
