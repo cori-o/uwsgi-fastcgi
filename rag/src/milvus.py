c4a3ea84b087bf05d0c9eb6c864793ceda4faded
Cori <ojd9512@gmail.com>
2025-03-21 13:40:02 +0900

    Update milvus.py

--- rag/src/milvus.py
+++ rag/src/milvus.py
@@ -12,7 +12,6 @@ class MilVus:
         self.ip_addr = db_config['ip_addr'] 
         self.port = db_config['port']
         self.set_env()
-
         if not MilVus._connected:
             self.set_env()
             MilVus._connected = True  # 연결 상태 업데이트
@@ -28,7 +27,6 @@ class MilVus:
                 return
         except Exception:
             pass  # 연결이 없으면 새로운 연결 생성
-
         self.conn = connections.connect(
             alias="default", 
             host= self.ip_addr,   # 'milvus-standalone', 
