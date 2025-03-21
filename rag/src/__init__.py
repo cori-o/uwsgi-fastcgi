40dec736cf5e837d52c4384724c7dbefa036715d
Finger-cori <ojd9512@gmail.com>
2025-03-21 14:52:38 +0900

    Update __init__.py
    delete MistralQT

--- rag/src/__init__.py
+++ rag/src/__init__.py
@@ -1,5 +1,5 @@
 from .llm import EmbModel, LLMOpenAI
-from .query import QueryTranslator, QueryRouter, OpenAIQT, MistralQT, RulebookQR
+from .query import QueryTranslator, QueryRouter, OpenAIQT, RulebookQR
 from .milvus import MilVus, MilvusEnvManager, DataMilVus, MilvusMeta
 from .data_p import DataProcessor
 from .pipe import EnvManager, ChatUser
