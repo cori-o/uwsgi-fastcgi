310bc61c0d4bc1b82ca196e2dd779f0248aa72d6
Finger-cori <ojd9512@gmail.com>
2025-03-21 14:36:23 +0900

    Update query.py
    remove MistralQT

--- rag/src/query.py
+++ rag/src/query.py
@@ -2,9 +2,8 @@
 Query Rewrititing Module (Query Decomposition + HyDE)
 '''
 from abc import ABC, abstractmethod
-from src import EmbModel, LLMOpenAI, LLMMistral
-from semantic_router import Route
-from semantic_router import RouteLayer
+from src import EmbModel, LLMOpenAI
+from semantic_router import Route, RouteLayer
 import os
 
 class QueryTranslator():
@@ -99,33 +98,6 @@ class OpenAIQT(QueryTranslator, LLMOpenAI):
         stepback_query = self.get_response(stepback_template, role="사용자 질문에 대해 간단 명료하게 답을 해줘.", model=model)
         return stepback_query
 
-
-class MistralQT(QueryTranslator, LLMMistral):
-    def __init__(self, config):
-        QueryTranslator.__init__(self, config)
-        LLMMistral.__init__(self, config)
-
-    def rewrite_query(self, query):
-        rewrite_template = f"""주어진 질문에 대해 더 나은 검색 쿼리를 제공해주세요. 검색 쿼리는 '**'로 끝나야 합니다.\n질문: {query} \n개선된 질문:"""
-        re_query = self.get_response(rewrite_template)
-        return re_query
-
-    def stepback_query(self, query):
-        stepback_template = f""" 당신은 금융권에서 종사하는 전문가입니다.\n당신의 역할은 질문을 보다 일반적인 형태로 바꿔서 좀 더 쉽게 대답할 수 있도록 하는 것입니다.
-            몇 가지 예시를 들어보면 다음과 같습니다.\n 
-            질문: 회사에 출근할 때 신분증을 가져가야 할까 ? 
-            일반적인 형태의 질문: 회사에 출근할 때 필요한게 뭐야 ?
-
-            질문: 국내로 출장갈 때 내가 지원받을 수 있는 금액은 얼마야 ?
-            일반적인 형태의 질문: 국내 출장여비 지급 기준에 대해 알려줘
-
-            위 예시를 참고하여 주어진 query를 간단하고 명료하게 재작성해주세요.
-
-            질문: {query}
-            일반적인 형태의 질문:
-            """
-        stepback_query = self.get_response(stepback_template)
-        return stepback_query
 
 class RulebookQR(QueryRouter, EmbModel):
     def __init__(self):
