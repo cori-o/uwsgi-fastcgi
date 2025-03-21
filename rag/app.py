81b2420e8504bb126caef95ab6adf944a3861269
Finger-cori <ojd9512@gmail.com>
2025-03-21 16:47:47 +0900

    Update app.py
    code refactoring

--- rag/app.py
+++ rag/app.py
@@ -1,56 +1,59 @@
-from pymilvus import Collection, CollectionSchema, FieldSchema, utility
 from flask import Flask, send_file, request, jsonify, Response
-from src import MilVus, DataMilVus, MilvusMeta
-from src import OpenAIQT, RulebookQR
-from src import EmbModel, LLMOpenAI
-from src import EnvManager, ChatUser
-import tempfile
+from pymilvus import Collection
+from dotenv import load_dotenv
+from src import EnvManager, DBManager
 import logging
-import json
-import os
+import json 
+import os 
 
-logger = logging.getLogger('stt_logger')
+logger = logging.getLogger('api_logger')
 logger.setLevel(logging.INFO)
-file_handler = logging.FileHandler('stt-result.log')
+file_handler = logging.FileHandler('api-result.log')
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
 
 app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+
+load_dotenv()
 args = dict()
 args['config_path'] = "./config"
 args['llm_config'] = "llm_config.json"
 args['db_config'] = "db_config.json"
-args['collection_name'] = "rule_book"
-env_manager = EnvManager(args)
-env_manager.set_config()
-print(env_manager)
+args['collection_name'] = "news"
+args['ip_addr'] = os.getenv('ip_addr')   
 
-data_milvus = env_manager.set_vectordb()
+env_manager = EnvManager(args)
+env_manager.set_processors()
+emb_model = env_manager.set_emb_model()
+milvus_data, milvus_meta = env_manager.set_vectordb()
 milvus_db = env_manager.milvus_db
-emb_model, response_model = env_manager.set_llm()
-print(emb_model, response_model)
+interact_manager = DBManager(data_p=env_manager.data_p, vectorenv=milvus_db, vectordb=milvus_data, emb_model=emb_model)
 
 
 @app.route('/data/show', methods=['GET'])
 def show_data():
+    '''
+    "collection_name": "news"   # news, description, etc ... 
+    '''
     collection_name = request.args.get('collection_name')
     if not collection_name:
         return jsonify({"error": "collection_name is required"}), 400
     
-    collection = Collection(collection_name)
-    milvus_db.get_partition_info(collection)
+    try:
+        assert Collection(collection_name)
+    except: 
+        response_data = {"error": "유효한 Collection 이름을 입력해야 합니다.", "collection list": milvus_db.get_list_collection()}
+        return Response(json.dumps(response_data, ensure_ascii=False), content_type="application/json; charset=utf-8")
+    
     milvus_db.get_collection_info(collection_name)
-    # 조회 결과를 JSON 형태로 반환
+    milvus_db.get_partition_info(collection_name)               
     return jsonify({
-        "collection_name": collection_name,
-        "partition_info": milvus_db.partition_names,
-        "collection_info": milvus_db.partition_entities_num
+        "schema": milvus_db.collection_schema.to_dict(),
+        "partition_names": milvus_db.partition_names,
+        "partition_nums": milvus_db.partition_entities_num,
     }), 200
-
-@app.route("/", methods=["GET"])
-def index():
-    return jsonify({"message": "Hello, FastCGI is working!"})
 
 @app.route('/data/insert', methods=['POST'])
 def insert_data():
@@ -73,6 +76,12 @@ def delete_data():
     '''
     data = request.json
     return jsonify({"status": "receviced"}), 200
+
+
+@app.route("/", methods=["GET"])
+def index():
+    return jsonify({"message": "Hello, FastCGI is working!"})
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
