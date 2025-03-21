e28bb79b227aa24751ef8ec241c9a0231bcf56dc
Finger-cori <ojd9512@gmail.com>
2025-03-21 17:41:22 +0900

    Update __init__.py

--- milvus-api/src/__init__.py
+++ milvus-api/src/__init__.py
@@ -1,4 +1,4 @@
 from .milvus import MilVus, MilvusEnvManager, DataMilVus, MilvusMeta
 from .data_p import DataProcessor
 from .models import EmbModel, LLMOpenAI
-from .pipe import EnvManager, DBManager
+from .pipes import EnvManager, DBManager
